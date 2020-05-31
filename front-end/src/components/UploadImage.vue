268b8d4ab8fd2628a66f9fb3da31314715352d18
Taras Rodionov <t.rodionov@admitad.com>
2020-05-31 18:04:30 +0300

    Add blurring

--- front-end/src/components/UploadImage.vue
+++ front-end/src/components/UploadImage.vue
@@ -13,26 +13,43 @@
                accept='image/*'
                @change='showPicked'>
       </div>
-      <button class='choose-file-button' type='button' @click="showPicker">Select Image</button>
+      <button v-if="!editImage" class='choose-file-button' type='button' @click="showPicker">Select Image</button>
       <div class='upload-label'>
         <label>{{ imageName ? imageName : 'No file chosen' }}</label>
       </div>
       <div>
         <img :class="imageUrl && !editImage ? '' : 'no-display'" alt='Chosen Image' height='400' :src="imageUrl">
       </div>
-      <cropper
-        v-if="editImage"
-        classname="cropper"
-        :src="imageUrl"
-        @change="crop"
-      ></cropper>
+
+      <div class="image-edit-panel" v-if="editImage">
+        <div class="image-field">
+          <cropper
+            v-if="mode === 'crop'"
+            classname="cropper"
+            :debounce="100"
+            :src="imageUrl"
+            @change="crop"
+          ></cropper>
+          <div :style="mode === 'blur' ? 'display: block' : 'display: none'">
+            <canvas ref="mainCanvas" width=300 height=300 @mousedown="handleMouseDown"></canvas>
+            <canvas ref="tempCanvas" width=300 height=300 class="tmp-canvas"></canvas>
+          </div>
+          <div class="apply-edit">
+            <button v-if="editImage" class='apply-button' type='button' @click="apply">
+              {{ mode === 'crop' ? 'Apply cropping' : 'Apply blurring' }}
+            </button>
+          </div>
+        </div>
+        <div class="tool-bar">
+          <img class="tool" src="@/assets/crop.png" alt="crop" title="Crop image" height="30" @click="mode = 'crop'">
+          <img class="tool" src="@/assets/blur.png" alt="blur" title="Blur image" height="30" @click="modeBlur">
+        </div>
+      </div>
+
       <div 
         class='result-label' 
         :style="'color: ' + responseMsgColor">
         <label>{{responseMsg}}</label>
-      </div>
-      <div class='result-label' v-if="editImage">
-        <label></label>
       </div>
       <div class='analyze'>
         <button v-if="!editImage" class='analyze-button' type='button' @click="analyze">{{ analyzeBtnTitle }}</button>
@@ -44,6 +61,7 @@
 
 <script>
 import { Cropper } from 'vue-advanced-cropper'
+import { boxBlurCanvasRGBA } from '@/assets/FastBlur.js'
 
 let dataURLtoBlob = require('blueimp-canvas-to-blob')
 
@@ -59,12 +77,68 @@ export default {
       analyzeBtnTitle: 'Analyze',
       resubmitBtnTitle: 'Resubmit',
       editImage: false,
+      mode: 'crop',
+      imgBlur: null,
     }
   },
   methods: {
+    apply () {
+      if (this.mode === 'crop') {
+        this.pickedImage = dataURLtoBlob(this.cropedImage)
+        this.imageUrl = URL.createObjectURL(this.pickedImage)
+      }
+    },
+    modeBlur () {
+      this.mode = 'blur'
+      
+      this.img = new window.Image()
+      this.img.crossOrigin = "anonymous"
+      this.img.src = this.imageUrl
+
+      this.img.onload = () => {
+        this.canvas = this.$refs.mainCanvas
+        this.tempCanvas = this.$refs.tempCanvas
+        this.tempCtx = this.tempCanvas.getContext("2d")
+        this.ctx = this.canvas.getContext("2d")
+
+        let width = this.img.width * (400 / this.img.height)
+        this.canvas.width = this.tempCanvas.width = width
+        this.canvas.height = this.tempCanvas.height = 400
+        this.ctx.drawImage(this.img, 0, 0, width, 400)
+
+        // let offsetX = canvas.offsetLeft;
+        // let offsetY = canvas.offsetTop;
+        // let scrollX = canvas.scrollLeft;
+        // let scrollY = canvas.scrollTop;
+        this.isDown = false;
+        // let PI2 = Math.PI * 2;
+      }
+    },
+    handleMouseDown (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      this.tempCtx.clearRect(0, 0, this.tempCanvas.width, this.tempCanvas.height);
+      this.isDown = true;
+    },
+    handleMouseUp(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      this.isDown = false;
+      this.tempCtx.save();
+      this.tempCtx.globalCompositeOperation = "source-in";
+      this.tempCtx.drawImage(this.img, 0, 0);
+      this.tempCtx.restore();
+      boxBlurCanvasRGBA("tempCanvas", 0, 0, this.tempCanvas.width, this.tempCanvas.height, 4, 0);
+      this.ctx.save();
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.drawImage(this.tempCanvas, 0, 0);
+      this.ctx.globalCompositeOperation = "destination-over";
+      this.ctx.drawImage(this.img, 0, 0);
+      this.ctx.restore();
+    },
     crop({coordinates, canvas}) {
       this.cropedImage = canvas.toDataURL()
-      console.log(this.cropedImage, coordinates)
+      console.log(coordinates)
     },
     showPicker () {
       const elem = this.$refs.fileInput
@@ -142,8 +216,9 @@ export default {
             (response) => {
               if (response.result === true) {
                 this.editImage = true
-                this.responseMsg = 'Crop out the regions that could reveal identity'
+                this.responseMsg = 'Crop out or blur the regions that could reveal identity'
                 this.responseMsgColor = '#262626'
+                this.mode = 'crop'
               } else {
                 this.responseMsg = 'No identity detected'
                 this.responseMsgColor = '#5ccb52'
@@ -191,7 +266,7 @@ body {
 }
 
 .analyze {
-    margin-top: 3em;
+    margin-top: 1em;
     margin-bottom: 5em;
 }
 
@@ -201,7 +276,7 @@ body {
 }
 
 .result-label {
-    margin-top: 0.5em;
+    margin-top: 3em;
     padding: 10px;
     font-size: 0.9rem;
     font-weight: bold;
@@ -244,7 +319,41 @@ button.resubmit-button {
     cursor: pointer;
 }
 
+button.apply-button {
+  margin-top: 0.5rem;
+  width: 130px;
+  height: 30px;
+  border: solid 1px #d1d1d1;
+  border-radius: 10px;
+  background-color: #d1d1d1;
+  font-size: 0.9rem;
+  color: #686868;
+  cursor: pointer;
+}
+
 button:focus {
     outline: 0;
+}
+
+.image-edit-panel {
+  margin-top: 1rem;
+  display: flex;
+  justify-content: center;
+}
+
+.tool-bar {
+  display: flex;
+  flex-direction: column;
+}
+
+.tool {
+  margin-left: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+}
+
+.tmp-canvas {
+  position: absolute;
+  top: -100%;
 }
 </style>
